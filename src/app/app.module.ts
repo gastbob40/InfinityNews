01772883eda5a6f1682ed76dbfda45e10cd81799
gastbob40 <gastbob40@gmail.com>
2020-09-17 20:02:40 +0200

    feat: add popover and storage module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {IonicStorageModule} from '@ionic/storage';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {PopoverController} from '@ionic/angular';
+import {PopoverComponent} from './popover/popover.component';
 
 @Component({
     selector: 'app-home',
@@ -7,10 +9,21 @@ import {Component} from '@angular/core';
 })
 export class HomePage {
 
-    constructor() {
+    constructor(public popoverController: PopoverController) {
     }
 
-    toggleDarkMode = () => {
+    async presentPopover(ev: any) {
+        console.log(ev);
+        const popover = await this.popoverController.create({
+            component: PopoverComponent,
+            event: ev,
+            translucent: true
+        });
+
+        return await popover.present();
+    }
+
+    toggleDarkMode() {
         document.body.classList.toggle('dark');
-    };
+    }
 }
