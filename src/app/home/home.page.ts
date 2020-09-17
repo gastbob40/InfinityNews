fb903b6cc9a6364ced276f4ad363b2d5b24e4746
gastbob40 <gastbob40@gmail.com>
2020-09-17 19:37:33 +0200

    feat: init project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    constructor() {
+    }
 
+    toggleDarkMode = () => {
+        document.body.classList.toggle('dark');
+    };
 }
