27661428f47b3947a97978e5230ee7e6ba55a0a5
gastbob40 <gastbob40@gmail.com>
2020-09-19 02:46:58 +0200

    feat: add function in ng manager to get selected newsgroup

--- src/app/services/newsgroup-manager.service.ts
+++ src/app/services/newsgroup-manager.service.ts
@@ -7,7 +7,9 @@ import {Storage} from '@ionic/storage';
 })
 export class NewsgroupManagerService {
     private STORAGE_NEWSGROUPS_KEY = 'newsgroups';
+    private STORAGE_SELECTED_NEWSGROUP_KEY = 'newsgroup';
     private newsgroups: NewsGroupInterface[] = null;
+    private selectedNewsgroup: NewsGroupInterface = null;
 
     constructor(private storage: Storage) {
     }
@@ -17,16 +19,29 @@ export class NewsgroupManagerService {
             this.newsgroups = await this.storage.get(this.STORAGE_NEWSGROUPS_KEY);
 
             if (this.newsgroups === null || this.newsgroups === undefined) {
-                await this.setNewsgroup([]);
+                await this.setNewsgroups([]);
             }
         }
 
         return this.newsgroups;
     }
 
-    async setNewsgroup(newsgroups: NewsGroupInterface[]) {
+    async getSelectedNewsgroup() {
+        if (this.selectedNewsgroup == null) {
+            this.selectedNewsgroup = await this.storage.get(this.STORAGE_SELECTED_NEWSGROUP_KEY);
+        }
+
+        return this.selectedNewsgroup;
+    }
+
+    async setNewsgroups(newsgroups: NewsGroupInterface[]) {
         this.newsgroups = newsgroups;
         await this.storage.set(this.STORAGE_NEWSGROUPS_KEY, this.newsgroups);
+    }
+
+    async setSelectedNewsgroup(newsgroup: NewsGroupInterface) {
+        this.selectedNewsgroup = newsgroup;
+        await this.storage.set(this.STORAGE_SELECTED_NEWSGROUP_KEY, this.selectedNewsgroup);
     }
 
 
