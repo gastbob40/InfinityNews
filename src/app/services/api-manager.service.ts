ee18c948dd84095d7016b972b8b4ce6bb8f598d6
gastbob40 <gastbob40@gmail.com>
2020-09-25 23:45:08 +0200

    feat: add environment variable

--- src/app/services/api-manager.service.ts
+++ src/app/services/api-manager.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
@@ -9,7 +10,7 @@ export class ApiManagerService {
     private readonly serverEndpoint: string;
 
     constructor(protected http: HttpClient) {
-        this.serverEndpoint = 'https://news-epita.gastbob40.ovh/api/';
+        this.serverEndpoint = environment.baseApiPath;
     }
 
     public doRequest(url: string): Observable<any> {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+    production: true,
+    baseApiPath: 'https://news-epita.gastbob40.ovh/api/'
 };
